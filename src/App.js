7874b8c737762f941657d837741bda7b584f9893
ianmcdavid <ianmcdavid25@gmail.com>
2021-06-20 15:01:57 -0700

    Added a second light switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
+import Switch from "./Switch";
 
 class App extends Component {
-  state = { on: true };
-
+  state = { on: false };
   toggle = () => {
     const isOn = this.state.on;
     this.setState({ on: !isOn });
@@ -14,13 +14,8 @@ class App extends Component {
     }
     return (
       <div className={wallClass}>
-        <div className="switch-plate">
-          <div className="screw" />
-          <div className="switch" onClick={this.toggle}>
-            <div className="switch-handle" />
-          </div>
-          <div className="screw" />
-        </div>
+        <Switch toggle={this.toggle} />
+        <Switch toggle={this.toggle} />
       </div>
     );
   }
